b9e79438a68f0fd87ff72d19ba9178c7cff6de6e
Jake Galligan <galligan1996@gmail.com>
2019-08-18 16:25:14 -0400

    Removing unnecessary imports in files

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
-import * as actions from './actions';
-import { connect } from "react-redux";
 
 const App = ({ children }) => {
   return (
