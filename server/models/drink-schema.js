ba7184117e257f927b10f229aa1f7b4d709679ab
Jake Galligan <galligan1996@gmail.com>
2019-08-20 14:09:24 -0400

    Create get route to get menu information back form server with query parameter that allows to specify drink category

--- server/models/drink-schema.js
+++ server/models/drink-schema.js
@@ -8,7 +8,7 @@ const DrinkSchema = new Schema ({
     Description: {type: String, required: true},
     Ounces: {type: Number, required: true},
     ABV: {type: Number, required: true},
-    Special:{type: Boolean, required: true}, 
+    Special:{type: Number, required: true}, 
 });
 
 module.exports = mongoose.model('Drink', DrinkSchema, 'drinklist');
--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const Drinks = require('../models/drink-schema');
+const DrinkList = require('../models/drink-schema');
 
+//create get route to retrieve drinks with optional query parameter to specify type of drink
 router.get('/', (req,res) => {
-    Drinks.find().exec((err,drinks) => {
-        console.log(drinks);
-        res.send(drinks);
-    })
+    let query = req.query.category;
+    let filter;
+    //if variable is not defined or specials set the query to search for the specials value 
+    //otherwise set the query to search for drink by category number
+    (!query || query == 'Specials') ? filter = {Special: 1}: filter = {Category: query} ;
+
+    DrinkList
+        .find(filter)
+        .exec((err,drinks) => {
+            if (err) {
+                res.status(400).send('Unable to get drinks make sure a valid query was entered');
+            }
+            res.send(drinks);
+        })
 
 })
 
