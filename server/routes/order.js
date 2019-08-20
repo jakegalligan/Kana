cd3218ee401c70cfb838af873b72d99b3dafb78b
Jake Galligan <galligan1996@gmail.com>
2019-08-20 15:37:18 -0400

    Add GET /order/:id route which allows client to get a specific order based off of an orders id

--- server/routes/order.js
+++ server/routes/order.js
@@ -12,7 +12,21 @@ router.post('/', (req,res) => {
     })
 })
 
-//create route which updates order
+    //create route which updates order
+
+//create route which gets individual order by id
+router.get('/:id', (req,res) => {
+    let id = req.params.id;
+    Order
+        .findById(id)
+        .exec((err, order) => {
+            if (err) {
+                res.status(400).send('Unable to find order');
+            } else {
+                res.send(order);
+            }
+        })
+})
 
 
 module.exports = router
