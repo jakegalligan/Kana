40d44cbb873c58bec0ddc1140c7d98de8950cc85
Jake Galligan <galligan1996@gmail.com>
2019-08-22 13:18:31 -0400

    Set basic card layout for orders in admin view

---
+++ server/client/src/components/admin/Order.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const Order = () => {
+  const classes = useStyles()
+    return (
+      <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be
+       
+          nev
+          
+          lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Claim</Button>
+        <Button size="small">Submit</Button>
+      </CardActions>
+    </Card>
+  );
+  };
+
+  export default Order;
+
+
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
--- server/client/src/components/admin/OrderBacklog.js
+++ server/client/src/components/admin/OrderBacklog.js
@@ -1,22 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client';
- const socket = io.connect('http://localhost:8000')
+import Order from './Order';
+const socket = io.connect('http://localhost:8000')
 
 const OrderBacklog = () => {
     const[orders,setOrders] = useState([])
     // listen for emissions from the server
         socket.on('list',(data) => {
-            console.log('hit backlog')
         //create new array with current and most recent order in order to avoid direct data manipulation
         let newArray = [...orders, data]
         //add the new order to the array
-        console.log(newArray);
         setOrders(newArray)
-        })
+    })
 
-    
+    const renderUnClaimedOrders = () => {
+        return orders.map(order => {
+            // <UnclaimedOrder
+
+            // >
+            // </UnclaimedOrder>
+        })
+    }
+        
     return (
         <div>
+            <Order />
             Hi
         </div>
     )
