e7c773fd58bead7e0d7bd103e78431dfba0a89ba
Jake Galligan <galligan1996@gmail.com>
2019-08-26 08:58:17 -0400

    Add styling to the app landing page

--- server/client/src/components/shared/LandingPage.js
+++ server/client/src/components/shared/LandingPage.js
@@ -1,17 +1,17 @@
 import React, {Fragment} from 'react';import {Redirect, Link} from 'react-router-dom';import styled from "styled-components";
 import Jumbotron from 'react-bootstrap/Jumbotron';import { makeStyles } from '@material-ui/styles';import Button from '@material-ui/core/Button';
 import HeaderBar from './HeaderBar';import uuidv1 from 'uuid';import Typography from '@material-ui/core/Typography';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserLock } from '@fortawesome/free-solid-svg-icons';import AboutInfo from './AboutInfo';import {Container, Row, Col} from 'react-bootstrap';
 
 const LandingPage = () => {
     //import styling for material ui components
     const classes = useStyles();
     return (
-        <Fragment>
-          <Link to ='/admin/login'>
-            <Typography className={classes.appTitle}>
-                Admin
+        <div className={classes.box}>
+          <Link to ='/admin/login' >
+            <Typography className={classes.admin}>
+                Admin <FontAwesomeIcon icon={faUserLock} />
             </Typography>
             </Link>
           <StyledDiv>
@@ -32,26 +32,29 @@ const LandingPage = () => {
               </Link>
           </StyledJumbotron>
           </StyledDiv>
-        </Fragment>
+        </div>
     )
   };
 
   //create styling for jumbotron
   const StyledJumbotron = styled(Jumbotron)`
-    height: 100vh;
     text-align: center;
+    border-style: solid;
+    border-color: white;
+    margin-top: 20%;
   `;
 
   const StyledDiv = styled(Container)`
   background: linear-gradient(to right top, #5c258d, #5e23a7, #5c22c3, #5222e0, #3826ff);
   background-repeat: no-repeat
   text-align: center;
+  height: 100vh;
 `;
 
     //create styline for button
 const useStyles = makeStyles({
     button: {  
-      background: 'linear-gradient(to right top, #5c258d, #5e23a7, #5c22c3, #5222e0, #3826ff)',
+      // background: 'linear-gradient(to right top, #5c258d, #5e23a7, #5c22c3, #5222e0, #3826ff)',
       border: 0,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(68, 0, 158, .3)',
@@ -70,7 +73,16 @@ const useStyles = makeStyles({
     },
     appTitle: {
       color: 'white',
-      background: 'none'
+      background: 'white'
+    },
+    box: {
+      background: 'linear-gradient(to right top, #5c258d, #5e23a7, #5c22c3, #5222e0, #3826ff)',
+    },
+    admin: {
+      color: 'white',
+      fontSize: '15px',
+      textAlign: 'right',
+      marginRight: '5%',
     },
     body: {
       color: 'white',
